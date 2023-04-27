5e70eb511cd13320bb0903121165987a43427ba8
xxftuu <xdemo1@163.com>
2023-04-28 02:26:40 +0800

    0000

---
+++ src/com/bd/bean/TimingRule.java
@@ -0,0 +1,13 @@
+package com.bd.bean;
+
+/**
+ * 
+ * @ClassName: TimingRule
+ * @author xxftuu
+ * @date 2023年4月28日
+ * @Description: 定时规则对象
+ *
+ */
+public class TimingRule {
+
+}
---
+++ src/com/bd/timing/cpu/TimingRuleParse.java
@@ -0,0 +1,14 @@
+package com.bd.timing.cpu;
+
+/**
+ * 
+ * @ClassName: TimingRuleParse
+ * @author xxftuu
+ * @date 2023年4月28日
+ * @Description: 定时规则解析器
+ *
+ */
+public class TimingRuleParse {
+	
+	
+}
